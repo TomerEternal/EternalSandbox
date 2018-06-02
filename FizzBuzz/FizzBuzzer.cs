85115ad46a6cdd93e929aa392774574ef879a3b4
Tomer Hirshfeld <Tomer.Hirshfeld@outlook.com>
2018-06-02 20:15:55 +0300

    Added fizzbuzz

---
+++ FizzBuzz/FizzBuzzer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz {
+    public class FizzBuzzer {
+        static void StartFizzBuzzing(int iterations, params (int divider, string name)[] rules) {
+            Enumerable.Range(0, iterations).ForEach(number => {
+                if (!rules.Any(r => {
+                    if (number % r.divider != 0)
+                        return false;
+                    else
+                        Console.Write(r.name);
+                    return true;
+
+                }))
+                    Console.Write(number);
+                Console.WriteLine();
+            });
+        }
+    }
+}
